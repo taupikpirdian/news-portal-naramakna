5cbaac84209c7e116ba9578db7cf84e2ea31814c
Taupik Pirdian <pirdiantaupik@gmail.com>
2026-02-13 16:11:40 +0700

    fix: grid layout for home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -586,10 +586,7 @@
         });
 
         html += `
-                        <div class="sm:hidden space-y-2">
-                            ${items}
-                        </div>
-                        <div class="hidden sm:grid grid-cols-2 gap-3">
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
                             ${items}
                         </div>
                     </div>
