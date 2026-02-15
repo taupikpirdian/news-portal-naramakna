20f53de44c7a0de8265b38b7b3bb2c18b2cdac4c
Taupik Pirdian <pirdiantaupik@gmail.com>
2026-02-15 14:41:18 +0700

    fix: update dummy person image to svg

--- resources/views/pages/tentang-kami.blade.php
+++ resources/views/pages/tentang-kami.blade.php
@@ -236,8 +236,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Waska Warta</div>
                     <div class="text-xs text-gray-600">Dewan Redaksi</div>
@@ -247,8 +248,9 @@
 
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Yosal Iriantara</div>
                     <div class="text-xs text-gray-600">Dewan Redaksi</div>
@@ -258,8 +260,9 @@
 
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Tansah Rahmatullah</div>
                     <div class="text-xs text-gray-600">Dewan Redaksi</div>
@@ -282,8 +285,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Yosal Iriantara</div>
                     <div class="text-xs text-gray-600">Pemimpin Redaksi</div>
@@ -306,8 +310,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Wahdi Suardi</div>
                     <div class="text-xs text-gray-600">Riset dan Analisis</div>
@@ -330,8 +335,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Khaerunnisa</div>
                     <div class="text-xs text-gray-600">Jurnalis</div>
@@ -340,8 +346,9 @@
         </div>
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Dadi Kurnia</div>
                     <div class="text-xs text-gray-600">Jurnalis</div>
@@ -364,8 +371,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363 h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Aas Fauziah</div>
                     <div class="text-xs text-gray-600">Editor Multimedia</div>
@@ -388,8 +396,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363 h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Dadi Kurnia</div>
                     <div class="text-xs text-gray-600">Account Executive</div>
@@ -412,8 +421,9 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="/assets/images/dummy-person.png">
+                <svg class="w-16 h-16 text-gray-800" fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363 h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/>
+                </svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Syamsul Ma'arif</div>
                     <div class="text-xs text-gray-600">Pemasaran dan Kerja Sama</div>
@@ -433,8 +443,7 @@
     <div class="grid md:grid-cols-3 gap-6">
         <div class="rounded-2xl border border-gray-200 p-6 bg-white hover:shadow-lg transition-all">
             <div class="flex items-center gap-4">
-                <img class="w-16 h-16 rounded-xl object-cover"
-                     src="{{ asset('assets/images/company-svgrepo-com.svg') }}">
+                <svg class="w-14 h-14 text-gray-800" viewBox="0 0 1024 1024" class="icon"  version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M531.8 385v483.3h0.1V385h-0.1z" fill="#343535" /><path d="M670.9 497.1h86v16h-86zM670.9 625.1h86v16h-86zM233.9 241.1h86v16h-86zM384 241.1h86v16h-86zM233.9 369h86v16h-86zM384 369h86v16h-86zM234 497.5h86v16h-86zM384 497.2h86v16h-86z" fill="#39393A" /><path d="M398.3 704.4c-11.9-11.9-28.4-19.3-46.5-19.3-36.2 0-65.8 29.6-65.8 65.8v117.4h20V750.9c0-12.2 4.8-23.6 13.5-32.3 8.7-8.7 20.2-13.5 32.3-13.5 12.2 0 23.6 4.8 32.3 13.5 8.7 8.7 13.5 20.2 13.5 32.3v117.4h20V750.9c0-18.1-7.4-34.5-19.3-46.5z" fill="#E73B37" /><path d="M575.8 429v437.9h0.1V429h-0.1zM286.2 868.3h131.6-131.6z" fill="#343535" /><path d="M896 868.3V385H575.9V111.6H128v756.7H64v44h896v-44h-64z m-364.1 0H172V155.6h359.9v712.7z m320.1-1.5H575.8V429H852v437.8z" fill="#39393A" /></svg>
                 <div>
                     <div class="text-sm font-semibold text-gray-900">Perusahaan</div>
                 </div>
@@ -447,8 +456,11 @@
         <div class="rounded-2xl border border-gray-200 p-6 bg-white hover:shadow-lg transition-all">
             <div class="flex items-center gap-3 mb-3">
                 <div class="flex items-center gap-4">
-                    <img class="w-16 h-16 rounded-xl object-cover"
-                        src="{{ asset('assets/images/address-svgrepo-com.svg') }}">
+                    <svg class="w-14 h-14 text-gray-800" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="currentColor" aria-hidden="true">
+                        <path d="M607.7 64.2c-102.9 0-186.3 83.4-186.3 186.3 0 30 15.9 66.6 38.3 103.3 15.2 24.8 33.3 49.7 51.7 72.6C559 486 607.7 533 607.7 533s17.7-17.1 42.3-43.8c14-15.3 30.3-33.6 46.7-53.8 47.9-58.7 97.3-132 97.3-184.9 0-102.9-83.4-186.3-186.3-186.3z m77.8 353.2c-8 10-16.1 19.6-23.9 28.6-7.8 9-15.4 17.5-22.5 25.3-12.5 13.7-23.4 25.1-31.4 33.3-12.5-12.8-32.3-33.5-53.8-58.5-7.8-9-15.8-18.5-23.8-28.5-13.5-16.7-25.5-32.7-36.1-48-10.1-14.6-18.8-28.4-26.1-41.5-17.6-31.6-26.6-57.8-26.6-77.8 0-44.4 17.3-86.2 48.7-117.6C521.4 101.3 563.2 84 607.6 84c44.4 0 86.2 17.3 117.6 48.7 31.4 31.4 48.7 73.2 48.7 117.6 0 20-8.9 46.1-26.5 77.6-14.8 26.9-35.6 56.9-61.9 89.5z"/>
+                        <path d="M607.7 183.1c36.9 0 67 30.1 67 67s-30.1 67-67 67-67-30.1-67-67 30-67 67-67m0-20c-48 0-87 38.9-87 87 0 48 38.9 87 87 87 48 0 87-38.9 87-87s-39-87-87-87z"/>
+                        <path d="M927.9 352.4l-195.7 70.3-35.6 12.8c-16.4 20.1-32.7 38.5-46.7 53.8l1.9 0.9v416.6l-12.7-5.9-46.3-21.6-212.2-98.9-8.6-4v-415l139.4 65c-18.4-23-36.5-47.8-51.7-72.6l-77.6-36.2-3.7-1.7-8.6-4-7.8-3.7-2.2 0.8L64 415.3v511.8L362 820l230.8 107.6 46.3 21.6 22.7 10.6L960 852.7V340.9l-32.1 11.5zM352 776.9l-4.9 1.8L108 864.6V446.2l244-87.7v418.4z m564 44.9l-244.2 87.7V491.1l5.1-1.8L916 403.4v418.4z"/>
+                    </svg>
                     <div>
                         <div class="text-sm font-semibold text-gray-900">Alamat Kantor</div>
                     </div>
@@ -462,8 +474,21 @@
         <div class="rounded-2xl border border-gray-200 p-6 bg-white hover:shadow-lg transition-all">
             <div class="flex items-center gap-3 mb-3">
                 <div class="flex items-center gap-4">
-                    <img class="w-16 h-16 rounded-xl object-cover"
-                        src="{{ asset('assets/images/contacts-svgrepo-com.svg') }}">
+                    <svg width="55" height="55" class="text-gray-800" fill="currentColor" viewBox="0 0 72 72" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <g>
+                        <path d="M56.068,4H15.932C12.104,4,9,7.104,9,10.932v50.136C9,64.896,12.104,68,15.932,68h40.136C59.896,68,63,64.896,63,61.068
+                            V10.932C63,7.104,59.896,4,56.068,4z M37.993,8h10v15.844l-4.563-2.188c-0.138-0.066-0.283-0.099-0.43-0.099
+                            s-0.3,0.033-0.436,0.099l-4.571,2.188V8z M13,61.068V10.932C13,9.313,14.313,8,15.932,8H22v56h-6.068
+                            C14.313,64,13,62.688,13,61.068z M59,61.068C59,62.688,57.688,64,56.068,64H24V8h11.993v17.432c0,0.344,0.18,0.663,0.471,0.847
+                            c0.291,0.183,0.658,0.205,0.968,0.055L43,23.666l5.568,2.667c0.137,0.067,0.285,0.099,0.432,0.099c0.186,0,0.368-0.052,0.529-0.153
+                            c0.289-0.184,0.464-0.503,0.464-0.847V8h6.075C57.688,8,59,9.313,59,10.932V61.068z"/>
+                        <path d="M56,51c-0.553,0-1,0.447-1,1v3.184C55,58.109,53.105,60,51.527,60H35.125c-0.553,0-1,0.447-1,1s0.447,1,1,1h16.402
+                            C54.32,62,57,59.325,57,55.184V52C57,51.447,56.553,51,56,51z"/>
+                        <path d="M55.245,47.951c0.26,0,0.521-0.102,0.71-0.29c0.18-0.19,0.29-0.44,0.29-0.71c0-0.262-0.111-0.521-0.302-0.71
+                            c-0.368-0.37-1.039-0.36-1.408,0c-0.182,0.188-0.29,0.448-0.29,0.71c0,0.27,0.108,0.529,0.29,0.71
+                            C54.725,47.85,54.975,47.951,55.245,47.951z"/>
+                    </g>
+                    </svg>
                     <div>
                         <div class="text-sm font-semibold text-gray-900">Kontak</div>
                     </div>
